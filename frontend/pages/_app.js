cd27b40b7331985acedeaa27a5fe389be8d3b571
Fation Krasniqi <fationkrasniqi777@gmail.com>
2023-01-05 17:34:43 +0100

    Auth added

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,21 @@
 import '../styles/globals.css'
 import {Provider,createClient} from "urql";
 import Nav from '../components/Nav';
-import {StateContext} from '../lib/context'
+import {StateContext} from '../lib/context';
+import {UserProvider} from '@auth0/nextjs-auth0/client';
 
 const client=createClient({url: process.env.NEXT_PUBLIC_BACKEND_API});
 
 function MyApp({ Component, pageProps }) {
   return (
+    <UserProvider>
     <StateContext>
        <Provider value={client}>
         <Nav/>
         <Component {...pageProps}/>
        </Provider>
        </StateContext>
+       </UserProvider>
   ) 
 }
 
